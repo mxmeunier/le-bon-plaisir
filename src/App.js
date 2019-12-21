99d572019ba2dc59588b49e89e103987e6797ef4
Maxime Meunier <mxmeunier@gmail.com>
2019-12-21 14:47:48 +0100

    add bootstrap 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 import HomePage from "./pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
